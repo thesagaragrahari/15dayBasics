f65d1232483239e33475abbc9a9edf7782c9d0e6
sagar agrahari <138549469+thesagaragrahari@users.noreply.github.com>
2026-09-13 15:40:54 +0000

    Complete Day 01 DSA - Two Sum

---
+++ day-01/DSA/TwoSum.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0;i<nums.length;i++){
+            for(int j = 0;j<nums.length;j++){
+                if(nums[i]+nums[j] == target && i != j)
+                    return new int[]{i,j};
+            }
+        }
+        return null;
+    }
+    public int[] twoSumOptimized(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i]))
+                return new int[]{map.get(target-nums[i]),i};
+            else
+                map.put(nums[i],i);
+        }
+        return null;
+    }
+}
