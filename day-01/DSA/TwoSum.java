30fd99072a5fb08605c956c94e5c025bfa17eb72
sagar agrahari <138549469+thesagaragrahari@users.noreply.github.com>
2026-09-13 16:21:29 +0000

    chatgpt marked day1 completed

--- day-01/DSA/TwoSum.java
+++ day-01/DSA/TwoSum.java
@@ -3,7 +3,7 @@ class Solution {
     public int[] twoSumBruteForce(int[] nums, int target) {
         for (int i = 0;i<nums.length;i++){
             for(int j = i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target && i != j)
+                if(nums[i]+nums[j] == target)
                     return new int[]{i,j};
             }
         }
