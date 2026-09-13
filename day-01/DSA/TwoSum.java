91093859faa85f2e340dfa30ba4dc29326f72421
sagar agrahari <138549469+thesagaragrahari@users.noreply.github.com>
2026-09-13 16:14:00 +0000

    After ChatGpt Review 1

--- day-01/DSA/TwoSum.java
+++ day-01/DSA/TwoSum.java
@@ -1,9 +1,8 @@
 class Solution {
 
     public int[] twoSumBruteForce(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0;i<nums.length;i++){
-            for(int j = 0;j<nums.length;j++){
+            for(int j = i+1;j<nums.length;j++){
                 if(nums[i]+nums[j] == target && i != j)
                     return new int[]{i,j};
             }
